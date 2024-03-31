01804691636c1715b205f2d1542ed5f121d434b1
NotGyashu <110993626+NotGyashu@users.noreply.github.com>
2024-03-31 23:38:40 +0530

    bug fixed

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -10,7 +10,7 @@ const Main = () => {
       {(write && <Write />) || <Read />}
 
       <div
-        class="fixed top-[70vh] right-[10vw] h-[3em] w-[3em] flex items-center justify-center rounded-full border cursor-pointer bg-[#703ba0] z-30"
+        class="fixed top-[85vh] md:top-[70vh] right-[10vw] h-[3em] w-[3em] flex items-center justify-center rounded-full border cursor-pointer bg-[#703ba0] z-30"
         onClick={() => {
           setWrite(!write);
         }}
--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -64,8 +64,8 @@ const Modal = ({ isOpen, onClose, post }) => {
       className={`fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 ${
         isOpen ? "" : "hidden"
       }`}
-     >
-      <form className="bg-white p-8 rounded shadow-md text-xl text-[#2d0d4a] w-[50vw] h-[60vh]  font-cookie relative">
+    >
+      <form className="bg-white lg:p-5 md:p-4 p-3 rounded shadow-md text-xl text-[#2d0d4a] lg:w-[50vw] md:w-[70vw]  w-[90vw] h-[60vh]  font-cookie relative">
         <div className="flex justify-between mb-4 ">
           <input
             className={`text-2xl font-bold w-full ${
@@ -117,8 +117,8 @@ const Modal = ({ isOpen, onClose, post }) => {
             ) : (
               <button
                 className="text-gray-600 absolute bottom-4 right-20 hover:text-gray-800"
-                onClick={(e)=>{
-                  e.preventDefault()
+                onClick={(e) => {
+                  e.preventDefault();
                   onClose();
                 }}
               >
